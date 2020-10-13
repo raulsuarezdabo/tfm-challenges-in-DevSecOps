c9aa1c6601da2297675c4dd9b887200c619df4b3
Raul Suarez Dabo <raul@suarez.com.es>
2020-10-13 10:32:13 +0200

    Adding initial structure

---
+++ src/main/java/uoc/edu/raulsuarez/devsecop/DevSecOpApplication.java
@@ -0,0 +1,13 @@
+package uoc.edu.raulsuarez.devsecop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSecOpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevSecOpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uoc/edu/raulsuarez/devsecop/DevSecOpApplicationTests.java
@@ -0,0 +1,13 @@
+package uoc.edu.raulsuarez.devsecop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSecOpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
