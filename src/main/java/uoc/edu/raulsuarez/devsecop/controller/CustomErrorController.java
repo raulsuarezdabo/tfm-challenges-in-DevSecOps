123b28d1712001ba22d1ba77a4b38bc05e2e860b
Raul Suarez Dabo <raul@suarez.com.es>
2020-10-14 12:48:56 +0200

    Adding css and style to be more pretty :)

--- src/main/java/uoc/edu/raulsuarez/devsecop/controller/CustomErrorController.java
+++ src/main/java/uoc/edu/raulsuarez/devsecop/controller/CustomErrorController.java
@@ -1,9 +1,14 @@
 package uoc.edu.raulsuarez.devsecop.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 public class CustomErrorController implements ErrorController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomErrorController.class);
 
     @Override
     public String getErrorPath() {
