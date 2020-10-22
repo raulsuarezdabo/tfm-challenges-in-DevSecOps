0435dcb92d51f107c003e45b4d5db618fed5f98c
Raul Suarez Dabo <raul@suarez.com.es>
2020-10-22 16:54:53 +0200

    Cleaning and ordering to feel more confortable

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,9 @@
 #!/usr/bin/env bash
 
-#PROJECT_ID="first-cluster-293016"
-#CLUSTER_ID="cluster-tfm-devsecop-travisci"
-#CLUSTER_ZONE="europe-west1-b"
-
-
-# Make sure we have gcloud installed in travis env
-#if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then
-#  rm -rf "$HOME/google-cloud-sdk"
-#  curl https://sdk.cloud.google.com | bash > /dev/null
-#fi
-
-# Promote gcloud to PATH top priority (prevent using old version from travis)
-#source $HOME/google-cloud-sdk/path.bash.inc
-
-# Make sure kubectl is updated to latest version
-#gcloud components update kubectl
-echo $KEY_FILE >> file.json
-gcloud auth activate-service-account --key-file file.json
+echo "$KEY_FILE" >> "$FILE_NAME"
+gcloud auth activate-service-account --key-file "$FILE_NAME"
 rm file.json
-gcloud container clusters get-credentials $CLUSTER_ID --zone $CLUSTER_ZONE --project $PROJECT_ID
+# shellcheck disable=SC2086
+gcloud container clusters get-credentials "$CLUSTER_ID" --zone "$CLUSTER_ZONE" --project $PROJECT_ID
 kubectl apply -f kube.yml
 
